8e4f1496938af5c49154fdd89fd5b71e2da1c851
opZylo <67260863+opZylo@users.noreply.github.com>
2022-07-16 00:57:34 -0500

    Update BudgetMaker.py

--- BudgetMaker.py
+++ BudgetMaker.py
@@ -9,11 +9,22 @@ while expNum != 0:
     expDict[expName] = 0
 
 for expName, v in expDict.items():
-    if v is 0:
+    if v == 0:
         print(expName)
         expDict[expName] = int(input('\nHow much did the following cost this month?: '))
+
+yearWageCheck = input('\nDo you know your annual wage? Yes or No? ')
+if yearWageCheck in ['Yes', 'yes', 'Y', 'y']:
+    yearWage = input('\nWhat is your annual wage? ')
+    print('Nice! your yearly wage is \n', yearWage)
+if yearWageCheck in ['No', 'no', 'N', 'n']:
+    yearWage = input('\nNo worries! What is your current hourly wage? ')
+    yearWage = yearWage * 2080
+    print('Nice! your yearly wage is \n', yearWage)
+elif yearWageCheck not in ['Yes', 'yes', 'Y', 'y', 'No', 'no', 'N', 'n']:
+    print('\nInvalid Response :/ ')
 
 
 
 print(sum(expDict.values()))
-print (expDict)
+print(expDict)
