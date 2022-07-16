6b66bcdcb7096d048aa29369b8005a005ae896c4
opZylo <67260863+opZylo@users.noreply.github.com>
2022-07-16 02:29:03 -0500

    Update BudgetMaker.py

--- BudgetMaker.py
+++ BudgetMaker.py
@@ -4,27 +4,55 @@ expNum = int(input('Enter the number of expenses you have in a month.\n '))
 expDict = {}
 
 while expNum != 0:
-    expName = input('Type the names of your current expenses hitting enter after each one.\n ')
+    expName = input('\nType the names of your current expenses hitting enter after each one.\n ')
     expNum = expNum - 1
     expDict[expName] = 0
 
 for expName, v in expDict.items():
     if v == 0:
         print(expName)
-        expDict[expName] = int(input('\nHow much did the following cost this month?: '))
+        expDict[expName] = int(input('\nHow much did the following cost this month?:\n '))
 
 yearWageCheck = input('\nDo you know your annual wage? Yes or No? ')
 if yearWageCheck in ['Yes', 'yes', 'Y', 'y']:
-    yearWage = input('\nWhat is your annual wage? ')
-    print('Nice! your yearly wage is \n', yearWage)
+    yearWage = float(input('\nWhat is your annual wage? '))
+    print('Nice! your yearly wage is \n$', yearWage)
 if yearWageCheck in ['No', 'no', 'N', 'n']:
-    yearWage = input('\nNo worries! What is your current hourly wage? ')
+    yearWage = float(input('\nNo worries! What is your current hourly wage? '))
     yearWage = yearWage * 2080
-    print('Nice! your yearly wage is \n', yearWage)
+    print('Nice! your yearly wage is \n$', yearWage)
 elif yearWageCheck not in ['Yes', 'yes', 'Y', 'y', 'No', 'no', 'N', 'n']:
     print('\nInvalid Response :/ ')
 
+payFreq = input('\nHow often do you get paid? (Weekly or Bi-Weekly) ')
+if payFreq in ['Weekly', 'weekly', 'W', 'w']:
+    yearWage = yearWage / 52
+    yearWage = yearWage * 4
+    print('\nSo you get paid Weekly, Nice! ')
+if payFreq in ['BiWeekly', 'Biweekly', 'biWeekly', 'biweekly', 'Bi-Weekly', 'Bi-weekly', 'bi-Weekly', 'bi-weekly', 'B', 'b']:
+    yearWage = yearWage / 26
+    yearWage = yearWage * 2
+    print('\nSo you get paid Bi-Weekly, Nice! ')
+elif payFreq not in ['BiWeekly', 'Biweekly', 'biWeekly', 'biweekly', 'Bi-Weekly', 'Bi-weekly', 'bi-Weekly', 'bi-weekly', 'B', 'b' 'Weekly', 'weekly', 'W', 'w']:
+    print('\nInvalid Response :/ ')
 
+savPer = input('\nThe reccomended savings amount per month is 10% of monthly earnings. Is this a good amount? ')
+if savPer in ['Yes', 'yes', 'Y', 'y']:
+    savPer = .10
+if savPer in ['No', 'no', 'N', 'n']:
+    savPer = float(input('\nWhat percentage would you like to save? '))
+    savPer = savPer / 100
 
-print(sum(expDict.values()))
-print(expDict)
+monExp = (sum(expDict.values()))
+savAmt = yearWage * savPer
+leftOver = yearWage - monExp - savAmt
+leftOverWeek = leftOver / 4
+
+print('\n-------------------------------\n')
+print('\nYour expense list is as follows\n', expDict)
+print('\n-------------------------------\n')
+print('Your mothly expense total is $', round(monExp,2) + round(savAmt,2), '\n')
+print('\n-------------------------------\n')
+print('\nYou are saving ', round(savAmt,2) ,' every month!')
+print('\n-------------------------------\n')
+print('\nOn a monthly basis, you have $', round(leftOver,2), 'Left over to work with for the month, or $', round(leftOverWeek,2), ' to left over to work with every week!')
